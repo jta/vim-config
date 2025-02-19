6a9f398ae01baff0172f8a976195220feb4862ea
Joao Taveira Araujo <joao.taveira@gmail.com>
2025-02-19 12:12:23 -0800

    Update cmp config
    Disable autocomplete

--- lua/vim-config/init.lua
+++ lua/vim-config/init.lua
@@ -92,22 +92,24 @@ require('Comment').setup()
 ---------------------------------------------------------------------
 -- nvim-cmp
 ---------------------------------------------------------------------
--- local cmp = require('cmp')
+local cmp = require('cmp')
 
--- cmp.setup({
-  -- mapping = cmp.mapping.preset.insert({
-      -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      -- ['<C-Space>'] = cmp.mapping.complete(),
-      -- ['<C-e>'] = cmp.mapping.abort(),
-      -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    -- }),
-    --['<Tab>'] = cmp.mapping.select_next_item(),
-    --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    --['<CR>'] = cmp.mapping.confirm({ select = true }),
-  --}),
-  -- sources = cmp.config.sources({
-    -- { name = 'nvim_lsp' }
-  -- })
--- })
+cmp.setup({
+  completion = {
+    autocomplete = false, -- Prevent automatic completion
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- Manually trigger completion with <C-Space>
+    ['<C-Space>'] = cmp.mapping.complete(),
 
+    -- Accept completion with <CR>
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+    -- Navigate the completion menu
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }),
+})

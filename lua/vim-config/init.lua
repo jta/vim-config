6bfd7bc5529a15f54f6626b62d6137ac96a84bc7
Joao Taveira Araujo <joao.taveira@gmail.com>
2025-02-19 11:57:59 -0800

    Update diagnostics

--- lua/vim-config/init.lua
+++ lua/vim-config/init.lua
@@ -7,6 +7,14 @@
 ---------------------------------------------------------------------
 local nvim_lsp = require('lspconfig')
 
+vim.diagnostic.config({
+  virtual_text = false,      -- Disable inline text errors
+  signs = true,              -- Show errors in the sign column
+  underline = true,          -- Enable underlining of errors
+  update_in_insert = false,  -- Prevent real-time updates
+  severity_sort = true,      -- Sort errors by severity
+})
+
 -- Function to enable signcolumn when LSP attaches
 local function enable_signcolumn()
   vim.opt.signcolumn = "yes"
@@ -38,25 +46,18 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   local opts = { noremap=true, silent=true }
 
-
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   buf_set_keymap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.set_loclist()<CR>', opts)
+  buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
 end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
@@ -87,11 +88,26 @@ require'nvim-treesitter.configs'.setup {
 ---------------------------------------------------------------------
 require('Comment').setup()
 
+
 ---------------------------------------------------------------------
--- Replace signs
+-- nvim-cmp
 ---------------------------------------------------------------------
-local signs = { Error = "✖ ", Warn = "⚠ ", Hint = "➤ ", Info = "ℹ " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
+-- local cmp = require('cmp')
+
+-- cmp.setup({
+  -- mapping = cmp.mapping.preset.insert({
+      -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      -- ['<C-Space>'] = cmp.mapping.complete(),
+      -- ['<C-e>'] = cmp.mapping.abort(),
+      -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    -- }),
+    --['<Tab>'] = cmp.mapping.select_next_item(),
+    --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    --['<CR>'] = cmp.mapping.confirm({ select = true }),
+  --}),
+  -- sources = cmp.config.sources({
+    -- { name = 'nvim_lsp' }
+  -- })
+-- })
+

324d9e6cb149965215e583630d85384c47cefbf6
João Taveira Araújo <joao@taveira.pt>
2025-04-15 10:05:53 -0700

    feat: enable clangd

--- lua/vim-config/init.lua
+++ lua/vim-config/init.lua
@@ -62,7 +62,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "gopls", "pyright" }
+local servers = { "gopls", "pyright", "clangd" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
